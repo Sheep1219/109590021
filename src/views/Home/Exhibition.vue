ba5c9c86e37ac7aa509e0300f32b8c61fa5527a6
MarcusRose22 <76154322+MarcusRose22@users.noreply.github.com>
2020-12-24 01:05:46 +0800

    Enter

---
+++ src/views/Home/Exhibition.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+
+<table>
+  <div>    
+    <tbody>
+        <tr>
+          <td class="img-pos"><img src="../../assets/banner.png"></td>
+          <td class="table-word"><h2>《那顆G1咻!》藝術學院學士班第一屆系展 </h2>
+          <h3>「當藝術碰撞科技，會迸出什麼樣的火花呢?」<br>
+              「科技藝術跨領域創作」一直以來為學士班重 <br>
+              要的創作方向，同學們將於系展中利用所學，<br>
+              展現創作熱誠與思維，分享作品中科技與藝術 <br>
+              接軌的威力！</h3></td>
+        </tr>
+    </tbody>
+  </div>
+</table>  
+
+
+
+  
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+html,body,#App{
+ width: 100%;
+ height: 100%;
+ margin: 0;
+ padding: 0;
+
+}
+
+.words{
+  color:lawngreen;
+  font-size: 80px;
+  align-content: center;
+  text-align: center;
+}
+
+table { 
+  position: relative;
+  /* border:2px solid #FFF; */
+  font-size:50px; 
+  width:fit-content;
+  text-align:center;
+  border-collapse:separate;
+  border-spacing:0px 50px;
+  word-break: break-all;
+  margin-left: 18.5%;  
+} 
+
+img{
+width:400px;
+height:300px; 
+}
+
+.table-word{
+  color:#FFF;
+  font-size:20px;
+}
+
+
+
+</style>
---
+++ src/views/Home/History.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+<div class="History"> <!-- title -->
+    <h1>藝術學院學士班歷史</h1>
+    <h2 class="context">⠀⠀⠀⠀清華大學2018年成立藝術學院〝科技藝術〞跨域「學士班」。「科技藝術（TechArt）」是當代藝術的新形式創作也是國際藝術發展的重要趨勢，包含虛擬藝術、互動藝術、電腦動畫、電腦音樂、聲音藝術、科技性表演藝術、混合藝術、網路社群、創客藝術、仿生藝術、生物藝術、人工生命…等。在當代數位美學與數位藝術的基礎下，我們結合創新科技，如各種互動感測技術、虛擬實境、物聯網、人工智慧、大數據分析、基因工程…，把「科技」與「藝術」結合視為「未來文化學」，培養前瞻性、跨域研究與創作人才。
+    </h2>
+</div>    
+</div>
+  
+
+  
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+.History
+{  
+  margin-top:80px;
+  font-family: Syncopate, "微軟正黑體", sans-serif;
+  text-align: center;
+  max-width: 50%;
+  margin-left: 24.5%;
+  color:#FFF;
+  
+}
+
+
+
+</style>
